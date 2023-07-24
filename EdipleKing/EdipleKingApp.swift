8c7b9774e9a100c8a7738e8ad65fb56e62556665
Gustavo Isoo Binder <gustan.binder@gmail.com>
2023-07-24 10:28:08 -0300

    Initial Commit

---
+++ EdipleKing/EdipleKingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdipleKingApp.swift
+//  EdipleKing
+//
+//  Created by Gustavo Binder on 24/07/23.
+//
+
+import SwiftUI
+
+@main
+struct EdipleKingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
